b1b8a9850b7b63c4796c4b10cd99414eaead2c2f
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-12-19 08:49:07 +0300

    realtime updates with graphql subscriptions

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./styles/index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 import { setContext } from "@apollo/client/link/context";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
 
 import {
   ApolloProvider,
   ApolloClient,
   createHttpLink,
   InMemoryCache,
+  split,
 } from "@apollo/client";
 import { BrowserRouter } from "react-router-dom";
 import { AUTH_TOKEN } from "./constants";
@@ -28,8 +31,27 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    },
+  },
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache(),
 });
 
